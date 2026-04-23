810b3ed18ab56b98c5ba4002cd503f547d54f6e1
Oleksandr Tovmach <alexandrtovmach@gmail.com>
2026-04-23 10:45:26 +0200

    fix: Updated version

--- scripts/copy-index.js
+++ scripts/copy-index.js
@@ -1,3 +1,10 @@
-import { copyFileSync } from 'fs';
+import { copyFileSync, readFileSync, writeFileSync } from 'fs';
+
 copyFileSync('dist/index.html', 'dist/404.html');
 console.log('404.html created');
+
+const { version } = JSON.parse(readFileSync('package.json', 'utf8'));
+const prlibPath = 'dist/prlib.html';
+const html = readFileSync(prlibPath, 'utf8');
+writeFileSync(prlibPath, html.replaceAll('__PRLIB_VERSION__', version));
+console.log(`prlib.html version injected: ${version}`);
